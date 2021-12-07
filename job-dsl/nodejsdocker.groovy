ae72d385e27ecf0465f4328e90742b3362ea1d4c
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-12-08 00:24:46 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
-job('NodeJS Docker example') {
+job('NodeJS Docker DSL') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/aashiqAhamedSP/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('aashiqAhamedSP')
+            node / gitConfigEmail('aashiqahamedsp@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('aashiqdevops/nodejshelloworld')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
